70bc50132a037b4b6420c6d262898c209ffc6c02
Zaki <emmanuelzaki.dasalla@gmail.com>
2023-09-04 01:27:09 +0800

    Goodjob

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is DASALLA, Emmmanuel Zaki!");
+    System.out.println("That is why developers are highly-paid!");
   }
 }  

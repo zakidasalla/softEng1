50f5a4b4d70f26f77ff7209d9704144c4e435d1e
Zaki <emmanuelzaki.dasalla@gmail.com>
2023-09-04 02:09:54 +0800

    Nice

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is DASALLA, Emmmanuel Zaki!");
-    System.out.println("That is why developers are highly-paid!");
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }  
